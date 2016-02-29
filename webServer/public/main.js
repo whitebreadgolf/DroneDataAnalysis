a4862f06e1218d6791455b1234f9b14e9a4dd039
Christine Song <songcy@usc.edu>
2016-02-28 23:17:12 -0800

    Adding interface updates

--- webServer/public/main.js
+++ webServer/public/main.js
@@ -1,6 +1,18 @@
-angular.module('UavOpsInterface', ['ngRoute'])
+angular.module('UavOpsInterface', ['ngRoute', 'nvd3'])
 	.config(function ($routeProvider) {
 		$routeProvider
+			.when('/speed', {
+				templateUrl: 'templates/speed.html',
+				controller: 'SpeedCtrl'
+			})
+			.when('/altitude', {
+				templateUrl: 'templates/altitude.html',
+				controller: 'AltitudeCtrl'
+			})
+			.when('/preflight', {
+				templateUrl: 'templates/preflight.html',
+				controller: 'PreFlightCtrl'
+			})
 			.when('/', {
 				templateUrl: 'templates/home.html',
 				controller: 'HomeCtrl'
@@ -8,4 +20,5 @@ angular.module('UavOpsInterface', ['ngRoute'])
 			.otherwise({
 				redirectTo: '/'
 			});
+
 	});
