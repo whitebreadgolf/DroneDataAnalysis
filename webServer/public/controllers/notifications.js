f7c7119e3c163bf60cf409da06dad88eb4bf02a2
Christine Song <songcy@usc.edu>
2016-03-06 18:04:25 -0800

    Notifications work... faux

--- webServer/public/controllers/notifications.js
+++ webServer/public/controllers/notifications.js
@@ -2,12 +2,13 @@ angular.module('UavOpsInterface')
 .controller('NotificationsCtrl', function($scope, Websocket, $interval){
 	$scope.greeting = "You've got Notifications!";
   
- //  	$interval(function (){
- //  		$scope.notifications = Websocket.getNotifications();
-	// }, 100); 
+  	$interval(function (){
+  		$scope.notifications = Websocket.getNotifications();
+	}, 100); 
 
  	$scope.isHideAlert = false;
-    $scope.alertMessage = "WARNING: _______";
+    //$scope.alertMessage = function(d){ return d.level; }
+	$scope.alertMessage = "WARNING: Drone altitude level is nearing FAA Regulation maximum.";
 
 });
 
