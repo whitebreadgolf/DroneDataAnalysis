cb8bd4c17dddab2aa18f3abf5baa8ada2fdcb85f
Christine Song <songcy@usc.edu>
2016-03-21 11:03:17 -0700

    History notes and configuration edits

--- webServer/public/controllers/history.js
+++ webServer/public/controllers/history.js
@@ -5,5 +5,16 @@ angular.module('UavOpsInterface')
   		$scope.altitudes = Websocket.getHistory();
 	}, 100); 
 
+//Account
+	//Flight [DATE] -> New page
+		//Configuration [TAB]
+		//Chart of entire flight [CHART] [TAB]
+		//Speed Notification (warning & breaches) [LIST] [TAB]
+		//Alt Notification (warning & breaches) [TAB]
+		
+		//Configure 
+	//Flight [DATE] -> New page
+		//Configuration
 
 });
+
