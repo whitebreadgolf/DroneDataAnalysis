dbc53d0ead10a331308eaa2add72460d7e93883f
Stephen Chen <chen293@usc.edu>
2016-03-21 19:47:32 -0700

    finally fixed magneticwarningfilter

--- webServer/app/analytics/magneticWarningFilter.js
+++ webServer/app/analytics/magneticWarningFilter.js
@@ -28,7 +28,7 @@ var gm_queue = [];
 @param {Number} magy - magnetic field data in the y plane
 @param {Number} magz - magnetic field data in the z plane
 */
-var magFilter = function(gyrox, gyroy, gyroz, magx, magy, magz){
+var magFilter = function(_id, gyrox, gyroy, gyroz, magx, magy, magz){
 
 	// create datapoint of magnetic and gyro data to add to queue
 	var magData = {
@@ -89,7 +89,7 @@ var magFilter = function(gyrox, gyroy, gyroz, magx, magy, magz){
 			level: 'hazard',
 			param: 'altitude',
 			text: 'Your drone is nearing sources of electromagnetic interference',
-			time: (new Date()) - regulationConfig.cur_flight[0].start_time
+			time: (new Date()) - regulationConfig.cur_flight[_id].start_time
 		};
 		wss.broadcast(JSON.stringify(warning));
 	}
